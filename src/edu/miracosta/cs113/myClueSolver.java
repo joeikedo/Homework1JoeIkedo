77582f3381ae531fcdb7824637869b44e003c2d4
Joe <joeikedo7@gmail.com>
2019-09-03 18:12:43 -0700

    Tentative completion

--- src/edu/miracosta/cs113/myClueSolver.java
+++ src/edu/miracosta/cs113/myClueSolver.java
@@ -1,13 +1,27 @@
 package edu.miracosta.cs113;
 
-import model.AssistantJack;
-import model.Theory;
+/**
+ * RandomClue.java : Your job is to ask your AssistantJack and get the correct
+ * answer in <= 20 tries.  RandomClue is ONE solution to the problem,
+ * where a set of random numbers is generated every attempt until all three
+ * random numbers match the solution from the AssistantJack object.
+ *
+ * This is a sample solution, a driver using random number implementation.
+ * You can use this file as a guide to create your own SEPARATE driver for
+ * your implementation that can solve it in <= 20 times consistently.
+ *
+ * @author Nery Chapeton-Lamas (material from Kevin Lewis)
+ * @version 1.0
+ *
+ */
 
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+import model.Theory;
+import model.AssistantJack;
+import java.util.ArrayList;
 
-public class myClueSolver {
+public class myClueSolver{
 
     /*
      * ALGORITHM:
@@ -51,20 +65,96 @@ public class myClueSolver {
         // PROCESSING
         jack = new AssistantJack(answerSet);
 
-        ArrayList<Integer> wrongWeapons = new ArrayList();
-        ArrayList<Integer> wrongLocations = new ArrayList();
-        ArrayList<Integer> wrongPeople = new ArrayList();
+        // Arrays
+        ArrayList<Integer> wrongWeapons = new ArrayList<Integer>();
+        ArrayList<Integer> wrongLocations = new ArrayList<Integer>();
+        ArrayList<Integer> wrongPeople = new ArrayList<Integer>();
 
         do {
-            weapon = random.nextInt(6) + 1;
-            //Need to check to see if the random weapon we rolled up for this iteration is on our wrongWeapons list!!
-            //If so, we need to roll up a new weapon.//change lol
+            // Here we initialize the weapon number based off our wrongWeapons array
+            if(wrongWeapons.size() == 0) {
+                weapon = random.nextInt(6) + 1;
+            }
+            else if(wrongWeapons.size() > 0 && wrongWeapons.size() < 5){
+                weapon = 1;
+
+                for(int i = 0; i<wrongWeapons.size(); i++) {
+
+                    if(wrongWeapons.contains(weapon)) {
+                        weapon = weapon + 1;
+                    }
+                    else {
+                        continue;
+                    }
+
+                }
+            }
+            else {
+                int sum = 0;
+                for(int i = 0; i < wrongWeapons.size(); i ++){
+                    sum = wrongWeapons.get(i) + sum;
+                }
+                weapon = 21 - sum; //We subtract by 21 because 1+2+3+4+5+6 is 21, thus the difference must be
+                //the remeaining weapon
+            }
+
+            // Here we initialize the location number based off our wrongLocations array
+            if(wrongLocations.size() == 0){
+                location = random.nextInt(10) + 1;
+            }
+            else if(wrongLocations.size() > 0 && wrongLocations.size() < 9){
+                location= 1;
+
+                for(int i = 0; i<wrongLocations.size(); i++) {
+
+                    if(wrongLocations.contains(location)) {
+                        location = location + 1;
+                    }
+                    else {
+                        continue;
+                    }
+
+                }
+            }
+            else{
+                int sum = 0;
+                for(int i = 0; i < wrongLocations.size(); i ++){
+                    sum = wrongLocations.get(i) + sum;
+                }
+                location= 55 - sum; //We subtract by 55 because 1+2+3+4+5+6+7+8+9+10 is 55, thus
+                // the difference must be the remaining location.
+            }
+
+            // Here we initialize the person number based off our wrongPeople array
+            if(wrongPeople.size() == 0){
+                murder = random.nextInt(6) + 1;
+            }
+            else if(wrongPeople.size() > 0 && wrongPeople.size() < 5){
+                murder= 1;
+                for(int i = 0; i<wrongPeople.size(); i++) {
+
+                    if(wrongPeople.contains(murder)) {
+                        murder = murder + 1;
+                    }
+                    else {
+                        continue;
+                    }
+
+                }
+            }
+            else{
+                int sum = 0;
+                for(int i = 0; i < wrongPeople.size(); i ++){
+                    sum = wrongPeople.get(i) + sum;
+                }
+                murder = 21 - sum; //We subtract by 21 because 1+2+3+4+5+6 is 21, thus the difference must be
+                //the remeaining person
+            }
 
 
-            location = random.nextInt(10) + 1;
-            murder = random.nextInt(6) + 1;     //As in, 'the murderer'
             solution = jack.checkAnswer(weapon, location, murder);
 
+            //For adding to the arrays
             if(solution == 1){
                 wrongWeapons.add(weapon);
             }
@@ -91,4 +181,3 @@ public class myClueSolver {
     }
 
 }
-
